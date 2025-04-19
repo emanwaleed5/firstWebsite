82308ff876cd3c5f19c54549d04a1081dc43f836
emanwaleed5 <ewaleed188@gmail.com>
2025-04-19 15:04:29 +0300

    Finished course 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const firstName ="Eman"
+/*const firstName ="Eman"
 const id = 1234
 const address = "Palestine, Gaza"
 const hobbies =["football","Dance"]
@@ -24,3 +24,35 @@ console.log(person.address.city)
 console.log(person.hobbies[0])
 person.firstName="Eman Waleed Eldeeb"
 console.log(person.firstName)
+console.log(person)
+*/
+/*const firstName ="Eman"
+const website = "EmanWaleed.com"
+console.log(`My name is ${firstName} and my website is ${website}`)
+alert("Amazing Job")
+window.addEventListener("resize", ()=>{console.log("Window resized")})
+const element =document.createElement( "h3")
+element.innerText= "2024"
+const title = document.getElementsByClassName("title");
+Array.from(title).forEach((element)=>{
+    element.style.color="blue"
+})
+console.log(document.getElementById("Jop"))
+console.log(document.querySelectorAll(".title"))
+*/
+
+const btn = document.querySelector("#clickhere")
+console.log(btn.dataset)
+btn.addEventListener("click", ()=>{
+    const clicks = btn.dataset.clicks;
+    btn.dataset.clicks=parseInt(btn.dataset.clicks) +1;
+    console.log(`clicked here ${clicks}`);
+}
+)
+
+btn.addEventListener("mouseenter", ()=>{
+    const clicks = btn.dataset.clicks;
+    btn.dataset.clicks=parseInt(btn.dataset.clicks) +1;
+    console.log(`mouse ented ${clicks}`);
+}
+)
